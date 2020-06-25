0d19922fd0f3ae6300623ef84297f4f61b338d22
Per Skullerud <pskullerud@hotmail.com>
2020-06-25 11:29:48 +0300

    2.0.2

--- private.js
+++
@@ -1,7 +0,0 @@
-const privateData = {
-    mongoAccess : 'mongodb+srv://Per:4A1avkiKvXLs1vRs@perliukai-4jawx.gcp.mongodb.net/data?retryWrites=true&w=majority',
-    APIkey : 'AIzaSyDPQzkTG2HwDbqJi-QzGPyzaDKEtyNk12k',
-    CSE : '013612111967417996789:xqs4jxjppnw'
-};
-
-module.exports = privateData;
